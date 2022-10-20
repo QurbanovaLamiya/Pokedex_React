cecd57c7b0ded4edbab4018b4ace8852bf5d72ae
QurbanovaLamiya <lamiyaqurbanova121@gmail.com>
2022-10-20 18:59:58 +0400

    Update folder structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component, Fragment } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Pokedex</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
